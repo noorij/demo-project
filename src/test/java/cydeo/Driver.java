ae808b1f49a042f10d25bc84dad495751863f82c
jawid <jawidahmada4@gmail.com>
2022-07-17 11:13:27 -0400

    chrome browser1

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "44.203.140.211";
+                        String gridAddress = "3.84.45.50";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
